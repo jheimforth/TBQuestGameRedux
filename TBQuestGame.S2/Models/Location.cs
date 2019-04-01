bac3ce33b4a23e41e48381c0c748d50e1fd3dd1f
jared heimforth <jheimforth78@live.com>
2019-03-31 21:07:22 -0400

    S2

---
+++ TBQuestGame.S2/Models/Location.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBQuestGame.S2.Models
+{
+    public class Location
+    {
+        #region FIELDS
+        private int _id;
+        private string _name;
+        private string _description;
+        private bool _accessible;
+        private int _modifyExperiencePoints;
+        private int _modifyHealth;
+
+
+        #endregion
+
+        #region PROPERTIES
+        public int ModifyHealth
+        {
+            get { return _modifyHealth; }
+            set { _modifyHealth = value; }
+        }
+
+
+        public int ModifyExperiencePoints
+        {
+            get { return _modifyExperiencePoints; }
+            set { _modifyExperiencePoints = value; }
+        }
+
+
+        public bool Accessible
+        {
+            get { return _accessible; }
+            set { _accessible = value; }
+        }
+
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+
+        #endregion
+
+        #region METHODS
+        public override string ToString()
+        {
+            return _name;
+        }
+
+        #endregion
+    }
+}
